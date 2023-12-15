6117216261e5d6963bbb5e50c2fdd0811e2066d1
ashwinilakshmanan <ashwinilakshmanan1@gmail.com>
2023-12-15 16:18:38 +0530

    created jwt token

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,39 +1,56 @@
-import bcrypt from 'bcrypt'
-import UserModel from '../models/UserModel.js'
+import bcrypt from "bcrypt";
+import UserModel from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
 
 //register user
-export const registerUser = async(req,res)=>{
-    const {name,email,password} = req.body;
+export const registerUser = async (req, res) => {
 
-    const salt = await bcrypt.genSalt(10)
-    const hashedPass =await bcrypt.hash(password,salt)
 
-    const newUser = new UserModel({name,email,password: hashedPass})
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPass;
+  const newUser = new UserModel(req.body);
+  const { email } = req.body;
+try{
+  const oldUser =await UserModel.findOne({ email});
 
-    try {
-        await newUser.save()
-        res.status(200).json(newUser)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
+  if (oldUser) {
+    return res.status(400).json({ message: "user is already registered!" });
+  }
+
+  const user = await newUser.save();
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user._id,
+    },
+    process.env.JWT_KEY,
+    { expiresIn: "1h" }
+  );
+ 
+  res.status(200).json({user,token});
+} catch (error) {
+  res.status(500).json({ message: error.message });
 }
+  
+};
 
 //login user
-export const loginUser = async(req,res)=>{
-    const {email,password} = req.body
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
-    try {
-
-        const user = await UserModel.findOne({email: email})
-        if(user){
-            const validity = await bcrypt.compare(password, user.password)
-            validity ? res.status(200).json(user) : res.status(400).json("wrong password")
-        }
-        else{
-            res.status(404).json("User does not exists")
-        }
-        
-    } catch (error) {
-        res.status(500).json({message: error.message})
+  try {
+    const user = await UserModel.findOne({ email: email });
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
+      validity
+        ? res.status(200).json(user)
+        : res.status(400).json("wrong password");
+    } else {
+      res.status(404).json("User does not exists");
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
