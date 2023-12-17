88acdffa019eeaf526e3df9b42f2214c29a18bb8
ashwinilakshmanan <ashwinilakshmanan1@gmail.com>
2023-12-17 13:03:26 +0530

    created update category api

--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { isAdmin, requireSignin } from "../middlewares/authMiddleware.js";
-import { createCategoryController } from "../controllers/createCategoryController.js";
+import {
+  createCategoryController,
+  updateCategoryController,
+} from "../controllers/CategoryController.js";
 
 const router = express.Router();
 
@@ -10,6 +13,14 @@ router.post(
   requireSignin,
   isAdmin,
   createCategoryController
+);
+
+//update category
+router.put(
+  "/update-category/:id",
+  requireSignin,
+  isAdmin,
+  updateCategoryController
 );
 
 export default router;
